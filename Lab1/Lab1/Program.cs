b4dae49a992725a26bee5ae3cc1b58f5b686eb8e
chatwarin <chatwarin97@gmail.com>
2017-10-24 19:30:50 +0700

    12 ex

--- Lab1/Lab1/Program.cs
+++ Lab1/Lab1/Program.cs
@@ -5,10 +5,24 @@ namespace Lab01
     {
         static void Main(string[] args)
         {
-            string str;
+            /*string str;
             Console.Write("Please enter value 1 : ");
             int val1 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine(val1);
+            Console.WriteLine(val1);*/
+            string name;
+            string lname;
+            int std;
+            float grade;
+
+            name = Console.ReadLine();
+            lname = Console.ReadLine();
+            int.TryParse(Console.ReadLine(), out std);
+            float.TryParse(Console.ReadLine(), out grade);
+
+            Console.WriteLine("Name: {0}", name);
+            Console.WriteLine("LastName: {0}", lname);
+            Console.WriteLine("Student ID: {0}", std);
+            Console.WriteLine("Grade: {0}", grade);
         }
     }
 }
