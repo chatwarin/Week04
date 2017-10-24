bacaaca80237143efd471fcd11e063f5b522f317
chatwarin <chatwarin97@gmail.com>
2017-10-24 14:51:51 +0700

    type + width of argument

--- Lab1/Lab1/Program.cs
+++ Lab1/Lab1/Program.cs
@@ -7,8 +7,13 @@ namespace Lab1
     {
         static void Main(string[] args)
         {
-            Console.Write("Hello");
-            Console.Write("Hello");
+            int n = 123456789;
+            Console.WriteLine("{0, 20:E}", 1);
+            Console.WriteLine("{0, 20:F}", 1);
+            Console.WriteLine("{0, 20:G}", 1);
+            Console.WriteLine("{0, 20:N}", 1);
+            Console.WriteLine("{0, 20:P}", 1);
+            Console.WriteLine("{0, 20:X}", 1);
         }
     }
 }
