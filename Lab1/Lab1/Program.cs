7809066910c7d239abb29e5555b4025462da49b4
chatwarin <chatwarin97@gmail.com>
2017-10-24 15:29:27 +0700

    input keyboard

--- Lab1/Lab1/Program.cs
+++ Lab1/Lab1/Program.cs
@@ -7,8 +7,10 @@ namespace Lab1
     {
         static void Main(string[] args)
         {
-            Console.Write("Hello");
-            Console.Write("Hello");
+            char ch;
+            Console.Write("Press a key followed by ENTER:");
+            ch = (char)Console.Read(); // get a char
+            Console.WriteLine("Your key is: " + ch);
         }
     }
 }
