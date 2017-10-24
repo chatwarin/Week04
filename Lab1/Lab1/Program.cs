ff3afbeac046945e189566218b1b2df6acb5b975
chatwarin <chatwarin97@gmail.com>
2017-10-24 14:54:20 +0700

    type + width of decimal

--- Lab1/Lab1/Program.cs
+++ Lab1/Lab1/Program.cs
@@ -7,8 +7,12 @@ namespace Lab1
     {
         static void Main(string[] args)
         {
-            Console.Write("Hello");
-            Console.Write("Hello");
+            const double i = 123.456789d;
+            Console.WriteLine("{0:F1}", i);
+            Console.WriteLine("{0:F2}", i);
+            Console.WriteLine("{0:F3}", i);
+            Console.WriteLine("{0:F4}", i);
+            Console.WriteLine("{0:F5}", i);
         }
     }
 }
