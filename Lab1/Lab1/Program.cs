3bb63723184394417b73106d0a04f7d3522756a0
chatwarin <chatwarin97@gmail.com>
2017-10-24 21:20:19 +0700

    random

--- Lab1/Lab1/Program.cs
+++ Lab1/Lab1/Program.cs
@@ -7,8 +7,14 @@ namespace Lab1
     {
         static void Main(string[] args)
         {
-            Console.Write("Hello");
-            Console.Write("Hello");
+            Random random = new Random();
+            int randomNumber = random.Next(0, 9);
+            Console.WriteLine(randomNumber);
+            Console.Write("Your number key:");
+            int Num = Convert.ToInt32(Console.ReadLine());
+            bool a = Num > randomNumber;
+            Console.WriteLine("Truth is : {0} > {1} ", Num, randomNumber);
+            Console.WriteLine("Truth is : {0}", a);
         }
     }
 }
