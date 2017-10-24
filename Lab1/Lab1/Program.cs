34b4244e076928211dfae6cf9e50a69094c7cb1c
chatwarin <chatwarin97@gmail.com>
2017-10-24 17:46:23 +0700

    logic NOR

--- Lab1/Lab1/Program.cs
+++ Lab1/Lab1/Program.cs
@@ -6,17 +6,17 @@ namespace thruthTable
         static void Main(string[] args)
         {
             bool A, B, Y,F;
-            Console.WriteLine("      Y = A NAND B");
+            Console.WriteLine("      Y = A NOR B");
             Console.WriteLine("-----------------------");
             Console.WriteLine("   A      B\t|  Y");
             Console.WriteLine("-----------------------");
-            A = false; B = false; F = A & B;Y = !F;
+            A = false; B = false; F = A | B;Y = !F;
             Console.WriteLine(" {0}\t{1}\t| {2}", A, B, Y);
-            A = false; B = true; F = A & B;Y = !F;
+            A = false; B = true; F = A | B;Y = !F;
             Console.WriteLine(" {0}\t{1}\t| {2}", A, B, Y);
-            A = true; B = false; F = A & B;Y = !F;
+            A = true; B = false; F = A | B;Y = !F;
             Console.WriteLine(" {0}\t{1}\t| {2}", A, B, Y);
-            A = true; B = true; F = A & B;Y = !F;
+            A = true; B = true; F = A | B;Y = !F;
             Console.WriteLine(" {0}\t{1}\t| {2}", A, B, Y);
             Console.WriteLine("-----------------------");
         }
