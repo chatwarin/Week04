b0bf8cd0e01ca112cc816e9412ce0b516d414862
chatwarin <chatwarin97@gmail.com>
2017-10-24 16:54:00 +0700

    logic AND

--- Lab1/Lab1/Program.cs
+++ Lab1/Lab1/Program.cs
@@ -1,14 +1,25 @@
 ﻿using System;
-
-
-namespace Lab1
+namespace thruthTable
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.Write("Hello");
-            Console.Write("Hello");
+            bool A, B, Y;
+            Console.WriteLine("      Y = A AND B");
+            Console.WriteLine("-----------------------");
+            Console.WriteLine("   A      B\t|  Y");
+            Console.WriteLine("-----------------------");
+            A = false; B = false; Y = A & B;
+            Console.WriteLine(" {0}\t{1}\t| {2}", A, B, Y);
+            A = false; B = true; Y = A & B;
+            Console.WriteLine(" {0}\t{1}\t| {2}", A, B, Y);
+            A = true; B = false; Y = A & B;
+            Console.WriteLine(" {0}\t{1}\t| {2}", A, B, Y);
+            A = true; B = true; Y = A & B;
+            Console.WriteLine(" {0}\t{1}\t| {2}", A, B, Y);
+            Console.WriteLine("-----------------------");
         }
     }
 }
+
